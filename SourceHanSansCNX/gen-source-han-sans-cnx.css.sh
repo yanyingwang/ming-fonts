bb98b7036b8b77933fb6d1df9fff5d8ec84b8092
Yanying Wang <yanyingwang1@gmail.com>
2023-04-03 00:01:45 +0800

    ming-font

---
+++ SourceHanSansCNX/gen-source-han-sans-cnx.css.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/bash
+
+echo "
+@font-face {
+     font-family: SourceHanSansCNX;
+     font-style: normal; /* italic oblique */
+     font-weight: normal; /* normal bold lighter bolder 100 900 */
+     font-stretch: normal; /* condensed expanded ultra-expanded 50% */
+     /* src: url('SourceHanSansCNX-Regular.woff2') format('woff2'); */
+     src: url('data:font/truetype;base64, $(base64 -w 0 SourceHanSansCNX-Regular.ttf)') format('truetype');
+     src: url('data:font/woff2;base64, $(base64 -w 0 SourceHanSansCNX-Regular.woff2)') format('woff2');
+}
+" > source-han-sans-cnx.css
+
--- build.ming-font.css.sh
+++ build.ming-font.css.sh
@@ -6,8 +6,7 @@ echo "
      font-style: normal; /* italic oblique */
      font-weight: normal; /* normal bold lighter bolder 100 900 */
      font-stretch: normal; /* condensed expanded ultra-expanded 50% */
-     /* src: url('SourceHanSansCNX-Regular.woff2') format('woff2'); */
-     src: url('data:font/woff2;base64, $(base64 -w 0 SourceHanSansCNX-Regular.woff2)') format('woff2');
+     src: url('data:font/truetype;base64, $(base64 -w 0 ming-regular.ttf)') format('truetype');
 }
-" > source-han-sans-cnx.css
+" > ming-font.css
 
